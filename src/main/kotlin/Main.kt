875570605352398d82490c39bcbdd784a7cc4f30
Andrey <andreu012a@mail.ru>
2026-03-07 16:53:00 +0700

    Добавил комментарии конфигурирования системы в src/main/kotlin/Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -13,12 +13,14 @@ import org.dru128.storage.PostgressWhiteList
 import org.dru128.vehicle.SimpleVehicleNumberValidator
 
 fun main() {
+    // Конфиг содержащий общию информацию
     val config = CheckpointConfig(
         openDuration = 2_000,
         loggerFilePath = "logs/checkpoint.txt",
         vehicleLoggerFilePath = "logs/vehicle.txt",
     )
 
+    // Универсльный логгер
     val logger = if (config.loggerFilePath != null) {
         FileLogger(config.loggerFilePath)
     } else {
@@ -27,20 +29,20 @@ fun main() {
 
     val checkPoint = SimpleCheckPoint(
         id = "CP-01",
-        vehicleIdentfier = ANPRCamera(),
-        barrier = BoomBarrier(logger = logger),
-        accessHandler = WhiteListAccessHandler(
-            whiteList = PostgressWhiteList,
-            numberValidator = SimpleVehicleNumberValidator(),
+        vehicleIdentfier = ANPRCamera(), // Идентифицирующее ТС устройство
+        barrier = BoomBarrier(logger = logger), // Ограничивающее проезд устройство
+        accessHandler = WhiteListAccessHandler( // алгоритм доступа - белый список
+            whiteList = PostgressWhiteList, // Имитация базы данных белого списка
+            numberValidator = SimpleVehicleNumberValidator(), // реализация простой валидации номера
         ),
-        openDuration = config.openDuration,
+        openDuration = config.openDuration, // время на к-е открывается проезд
         logger = logger,
     )
 
     // прокси КПП, добавляющий логирование событий факта проезда/отказа в доступе и статусов
     val loggedCheckPoint = LoggerProxyCheckpoint(
         checkPoint = checkPoint,
-        logger = ConsoleVehicleLogger(),
+        logger = ConsoleVehicleLogger(), // консольный логгер событий проезда
     )
 
     loggedCheckPoint.start()
